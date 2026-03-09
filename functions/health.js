4c7eeb5951de073b2c0324fd681906653dd32644
itamarbraga <itamarbraga@gmail.com>
2026-03-09 17:52:07 +0100

    Delete functions directory

--- functions/health.js
+++
@@ -1,7 +0,0 @@
-exports.handler = async function () {
-  return {
-    statusCode: 200,
-    headers: { 'content-type': 'application/json; charset=utf-8' },
-    body: JSON.stringify({ ok: true, service: 'brazuca-fc-v3', timestamp: new Date().toISOString() })
-  };
-};
